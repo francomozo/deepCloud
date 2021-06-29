f387508b676eab92b8380130cf6502e185c80fa9
Andres Herrera <97.andres.herrera@gmail.com>
2021-06-29 00:27:22 -0300

    new train model with 1 val per epoch

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ import numpy as np
 import optuna
 import torch
 import torch.nn as nn
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from src.lib.utils import print_cuda_memory
 
@@ -23,7 +24,8 @@ def train_model(model,
                 checkpoint_every=None,
                 verbose=True,
                 eval_every=100,
-                writer = None):
+                writer=None,
+                sheduler=None):
 
     # TODO: - save best acc model DONE(revision pending)
     #       - decide what the function returns
@@ -119,6 +121,7 @@ def train_model(model,
                 f'Time elapsed in current epoch: {(end_epoch - start_epoch):.2f} secs.')
 
         if CURRENT_VAL_ACC < BEST_VAL_ACC:
+            BEST_VAL_ACC = CURRENT_VAL_ACC
             model_dict = {
                 'epoch': epoch + 1,
                 'model_state_dict': model.state_dict(),
@@ -374,6 +377,7 @@ def train_modelSSIM(model,
                 f'Time elapsed in current epoch: {(end_epoch - start_epoch):.2f} secs.')
 
         if CURRENT_VAL_ACC < BEST_VAL_ACC:
+            BEST_VAL_ACC = CURRENT_VAL_ACC
             model_dict = {
                 'epoch': epoch + 1,
                 'model_state_dict': model.state_dict(),
@@ -404,3 +408,101 @@ def weights_init(model):
 
 # 1) load model , ex: model = UNet(...)
 # 2) model.apply(weights_init)
+
+def train_model_2(model,
+                criterion,
+                optimizer,
+                device,
+                train_loader,
+                epochs,
+                val_loader,
+                checkpoint_every=None,
+                verbose=True,
+                writer=None,
+                scheduler=None):
+    
+    TRAIN_LOSS_GLOBAL = [] #perists through epochs, stores the mean of each epoch
+    VAL_LOSS_GLOBAL = []
+
+    TIME = []
+
+    BEST_VAL_ACC = 1e5
+        
+    for epoch in range(epochs):
+        start_epoch = time.time()
+        TRAIN_LOSS_EPOCH = [] #stores values inside the current epoch
+
+        for batch_idx, (in_frames, out_frames) in enumerate(train_loader):
+            model.train()
+
+            in_frames = in_frames.to(device=device)
+            out_frames = out_frames.to(device=device)
+
+            # forward
+            frames_pred = model(in_frames)
+            loss = criterion(frames_pred, out_frames)
+
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            # gradient descent or adam step
+            optimizer.step()
+
+            TRAIN_LOSS_EPOCH.append(loss.detach().item())
+            
+        TRAIN_LOSS_GLOBAL.appens(sum(TRAIN_LOSS_EPOCH)/len(TRAIN_LOSS_EPOCH))
+        
+        #evaluation
+        model.eval()
+
+        with torch.no_grad():
+            VAL_LOSS_EPOCH = []
+            for val_batch_idx, (in_frames, out_frames) in enumerate(val_loader):
+                in_frames = in_frames.to(device=device)
+                out_frames = out_frames.to(device=device)
+
+                frames_pred = model(in_frames)
+                val_loss = criterion(frames_pred, out_frames)
+
+                VAL_LOSS_EPOCH.append(val_loss.detach().item())
+                
+        VAL_LOSS_GLOBAL.append(sum(VAL_LOSS_EPOCH)/len(VAL_LOSS_EPOCH))
+        
+        if scheduler:
+            scheduler.step(VAL_LOSS_GLOBAL[-1])
+        
+        end_epoch = time.time()
+        TIME = end_epoch - start_epoch
+        
+        if verbose:
+            # print statistics
+            print(f'Epoch({epoch + 1}/{epochs}) | ', end='')
+            print(f'Train_loss({(TRAIN_LOSS_GLOBAL[-1]):06.4f}) | Val_loss({VAL_LOSS_GLOBAL[-1]:.2f})', end='')
+            print(f'Time_per_batch({TIME:.2f}s)') # this part maybe dont print
+                    
+        if writer: 
+            #add values to tensorboard 
+            writer.add_scalar("TRAIN LOSS, EPOCH MEAN",TRAIN_LOSS_GLOBAL[-1], epoch)
+            writer.add_scalar("VALIDATION LOSS, EPOCH MEAN" , VAL_LOSS_GLOBAL[-1] , epoch)
+
+        if VAL_LOSS_GLOBAL[-1] < BEST_VAL_ACC:
+            BEST_VAL_ACC = VAL_LOSS_GLOBAL[-1]
+            model_dict = {
+                'epoch': epoch + 1,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss_epoch_mean': TRAIN_LOSS_GLOBAL[-1],
+                'val_loss_epoch_mean': VAL_LOSS_GLOBAL[-1]
+            }
+        else:
+            model_dict = None
+
+        if checkpoint_every is not None and (epoch + 1) % checkpoint_every == 0:
+            PATH = 'checkpoints/'
+            ts = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
+            NAME = 'model_epoch' + str(epoch + 1) + '_' + str(ts) + '.pt'
+
+            torch.save(model_dict, PATH + NAME)
+            
+    return TRAIN_LOSS_GLOBAL, VAL_LOSS_GLOBAL
