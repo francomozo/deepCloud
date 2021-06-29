843323323c4f4cd6aff04e8bce0eba2b3dbfaa37
Andres Herrera <97.andres.herrera@gmail.com>
2021-06-28 22:38:27 -0300

    correction

--- src/train.py
+++ src/train.py
@@ -395,7 +395,7 @@ def train_modelSSIM(model,
 
 def weights_init(model):
     if isinstance(model, nn.Conv2d):
-        torch.nn.init.xavier_normal_(m.weight)
+        torch.nn.init.xavier_normal_(model.weight)
         if model.bias is not None:
           nn.init.constant_(model.bias.data, 0)
 
