b11fb9502945e3dcd9f78211ff275756b98b87ff
Franco Mozo <fm.login1@gmail.com>
2021-08-13 17:06:53 -0300

    :cyclone: gan modified

--- gan.py
+++ gan.py
@@ -1,22 +1,26 @@
+import random
+
 import torch
 import torch.optim as optim
+import torchvision
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-import torchvision
+
 from src import preprocessing, train
 from src.data import MontevideoFoldersDataset
 from src.dl_models.unet import UNet2
 
-# Hyperparams
+# Paras and hyperparams
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 torch.manual_seed(50)
-BATCH_SIZE = 4
 PT_PATH = '/clusteruy/home03/DeepCloud/deepCloud/checkpoints/10min_UNet2_sigmoid_mae_f32_60_04-08-2021_20:43.pt'
 CSV_PATH='/clusteruy/home03/DeepCloud/deepCloud/data/mvd/train_cosangs_in3_out1.csv'
 LEARNING_RATE = 1e-4
+BATCH_SIZE = 4
 NUM_EPOCHS = 100
-#CRITIC_ITERATIONS = None
 LAMBDA_GP = 10
+#CRITIC_ITERATIONS = 5
+
 
 # utils.py
 def gradient_penalty(disc, real, fake, device="cpu"):
@@ -42,7 +46,6 @@ def gradient_penalty(disc, real, fake, device="cpu"):
 
 # Dataloaders
 normalize = preprocessing.normalize_pixels()
-
 train_ds = MontevideoFoldersDataset(path='/clusteruy/home03/DeepCloud/deepCloud/data/mvd/train/',    
                                      in_channel=3,
                                      out_channel=1,
@@ -55,9 +58,10 @@ train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_wor
 # Nets
 gen = UNet2(n_channels=3, n_classes=1, bilinear=True, filters=32).to(device)
 disc = UNet2(n_channels=1, n_classes=1, bilinear=True, filters=32).to(device)
-gen.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
+# gen.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
 #disc.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
-disc.apply(train.weights_init)
+#gen.apply(train.weights_init)
+#disc.apply(train.weights_init)
 
 # Initializate optimizer
 opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
@@ -67,16 +71,16 @@ gen.train()
 disc.train()
 
 # tb
-writer_real = SummaryWriter(f"runs/gan/gt")
-writer_fake = SummaryWriter(f"runs/gan/pred")
+writer_gt = SummaryWriter(f"runs/gan/gt")
+writer_pred = SummaryWriter(f"runs/gan/pred")
 step = 0
 
 for epoch in range(NUM_EPOCHS):
-    # for batch_idx, (real, _) in enumerate(train_loader):
-    for batch_idx, (in_frames, out_frames) in enumerate(train_loader):
+    for batch_idx, (in_frames, gt) in enumerate(train_loader):
+        
         in_frames = in_frames.to(device) # this is the noise (B, C, H, W)=(1, 3, 256, 256)
-        out_frames = out_frames.to(device) # this is the real (1, 1, 256, 256))
-        cur_batch_size = out_frames.shape[0]
+        gt = gt.to(device) # this is the real (1, 1, 256, 256))
+        cur_batch_size = gt.shape[0]
         # HERE THE PAPER TRAINS A FEW ITER THE DISC BUT I WONT DO THAT
         # Train Critic: max E[critic(real)] - E[critic(fake)]
         # equivalent to minimizing the negative of that
@@ -84,19 +88,20 @@ for epoch in range(NUM_EPOCHS):
         #noise = torch.randn(cur_batch_size, Z_DIM, 1, 1).to(device)
         
         pred = gen(in_frames) # (1, 1, 256, 256)
-        disc_real = disc(out_frames).reshape(-1)
-        disc_fake = disc(pred).reshape(-1)
-        gp = gradient_penalty(disc, out_frames, pred, device=device)
-        loss_critic = (
-            -(torch.mean(disc_real) - torch.mean(disc_fake)) + LAMBDA_GP * gp
+        
+        disc_pred = disc(pred).reshape(-1)
+        disc_gt = disc(gt).reshape(-1)
+        gp = gradient_penalty(disc, gt, pred, device=device)
+        loss_disc = (
+            -(torch.mean(disc_gt) - torch.mean(disc_pred)) + LAMBDA_GP * gp
         )
         disc.zero_grad()
-        loss_critic.backward(retain_graph=True)
+        loss_disc.backward(retain_graph=True)
         opt_disc.step()
 
-        # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
-        gen_fake = disc(pred).reshape(-1)
-        loss_gen = -torch.mean(gen_fake)
+        # Train Generator: max E[disc(gen_noise)] <-> min -E[disc(gen_noise)]
+        gen_pred = disc(pred).reshape(-1)
+        loss_gen = -torch.mean(gen_pred)
         gen.zero_grad()
         loss_gen.backward()
         opt_gen.step()
@@ -105,14 +110,13 @@ for epoch in range(NUM_EPOCHS):
         if batch_idx % 100 == 0 and batch_idx > 0:
             print(
                 f"Epoch [{epoch}/{NUM_EPOCHS}] Batch {batch_idx}/{len(train_loader)} \
-                  Loss D: {loss_critic:.4f}, loss G: {loss_gen:.4f}"
+                  Loss D: {loss_disc:.4f}, loss G: {loss_gen:.4f}"
             )
 
             with torch.no_grad():
-                img_grid_real = torchvision.utils.make_grid(out_frames, normalize=True)
-                img_grid_fake = torchvision.utils.make_grid(pred, normalize=True)  
+                img_grid_gt = torchvision.utils.make_grid(gt, normalize=True)
+                img_grid_pred = torchvision.utils.make_grid(pred, normalize=True)  
                 
-                writer_real.add_image("gt", img_grid_real, global_step=step)
-                writer_fake.add_image("pred", img_grid_fake, global_step=step)
-
+                writer_gt.add_image("gt", img_grid_gt, global_step=step)
+                writer_pred.add_image("pred", img_grid_pred, global_step=step)
             step += 1
