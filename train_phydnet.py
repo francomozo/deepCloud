7d063cd115ff11b9fed32e9644e75a5ee986cca5
Andres Herrera <97.andres.herrera@gmail.com>
2021-08-13 00:22:09 -0300

    writer and small changes

--- train_phydnet.py
+++ train_phydnet.py
@@ -22,7 +22,7 @@ import argparse
 import matplotlib.pyplot as plt
 
 
-def train_on_batch(input_tensor, target_tensor, encoder, encoder_optimizer, criterion, teacher_forcing_ratio):                
+def train_on_batch(input_tensor, target_tensor, encoder, encoder_optimizer, criterion, teacher_forcing_ratio, writer=None):                
     encoder_optimizer.zero_grad()
     # input_tensor : torch.Size([batch_size, input_length, channels, cols, rows])
     input_length  = input_tensor.size(1)
@@ -58,7 +58,7 @@ def train_on_batch(input_tensor, target_tensor, encoder, encoder_optimizer, crit
     encoder_optimizer.step()
     return loss.item() / target_length
 
-def trainIters(train_loader, val_loader, encoder, nepochs, print_every=10, eval_every=10, model_name=''):
+def trainIters(train_loader, val_loader, encoder, nepochs, print_every=10, eval_every=10, checkpoint=False, model_name='', writer=None):
     train_losses = []
     best_mse = float('inf')
 
@@ -77,38 +77,48 @@ def trainIters(train_loader, val_loader, encoder, nepochs, print_every=10, eval_
             
             loss = train_on_batch(input_tensor[:,:,:,:,:], target_tensor[:,:,:,:,:], encoder, encoder_optimizer, criterion, teacher_forcing_ratio)                                   
             loss_epoch += loss
+            
+        if writer: 
+            #add values to tensorboard 
+            writer.add_scalar("TRAIN LOSS", loss_epoch, epoch)             
                       
         train_losses.append(loss_epoch)        
         if (epoch+1) % print_every == 0:
-            print('epoch ', epoch,  ' loss ', loss_epoch, ' time epoch ', time.time()-t0)
+            print('epoch ', epoch,  ' loss ', loss_epoch, 'train time epoch ', time.time()-t0)
             
         if (epoch+1) % eval_every == 0:
-            mse, mae, ssim = evaluate(encoder, val_loader) 
+            mse, mae, ssim = evaluate(encoder, val_loader, writer) 
             scheduler_enc.step(mse)
-            if mse < best_mse:
-                best_mse = mse
-                model_dict = {
-                    'epoch': epoch + 1,
-                    'model_state_dict': encoder.state_dict(),
-                    'optimizer_state_dict': encoder_optimizer.state_dict(),
-                    'train_loss_epoch_mean': loss_epoch,
-                    'val_loss_mae': mae,
-                    'val_loss_mse': mse,
-                    'val_loss_ssim': ssim,
-                }   
-                PATH = 'checkpoints/'
-                ts = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
-                NAME =  model_name + '_' + str(epoch + 1) + '_' + str(ts) + '.pt'
-
-                torch.save(model_dict, PATH + NAME)  
+            if writer: 
+                writer.add_scalar("VAL MSE", mse, epoch)
+                writer.add_scalar("VAL MAE", mae, epoch)
+                writer.add_scalar("VAL SSIM", ssim, epoch)
+                writer.add_scalar("Learning rate", encoder_optimizer.state_dict()["param_groups"][0]["lr"], epoch)
+                
+            if checkpoint:    
+                if mse < best_mse:
+                    best_mse = mse
+                    model_dict = {
+                        'epoch': epoch + 1,
+                        'model_state_dict': encoder.state_dict(),
+                        'optimizer_state_dict': encoder_optimizer.state_dict(),
+                        'train_loss_epoch_mean': loss_epoch,
+                        'val_loss_mae': mae,
+                        'val_loss_mse': mse,
+                        'val_loss_ssim': ssim,
+                    }   
+                    PATH = 'checkpoints/'
+                    ts = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
+                    NAME =  model_name + '_' + str(epoch + 1) + '_' + str(ts) + '.pt'
+                    torch.save(model_dict, PATH + NAME)  
                           
     return train_losses
 
-def evaluate(encoder, loader):
-    total_mse, total_mae,total_ssim,total_bce = 0,0,0,0
+def evaluate(encoder, val_loader, writer=None):
+    total_mse, total_mae, total_ssim,total_bce = 0,0,0,0
     t0 = time.time()
     with torch.no_grad():
-        for i, (in_frames, out_frames) in enumerate(loader):
+        for i, (in_frames, out_frames) in enumerate(val_loader):
             input_tensor = in_frames.unsqueeze(2).to(device=device)
             target_tensor = out_frames.unsqueeze(2).to(device=device)
             
@@ -126,20 +136,23 @@ def evaluate(encoder, loader):
                                                                            first_timestep=False,
                                                                            decoding=False)
                 decoder_input = output_image
+                if writer and (i == 0):
+                    writer.add_images('predictions_batch', frames_pred[:5, 0, 0], epoch)
+                    
                 predictions.append(output_image.cpu())
 
             input = input_tensor.cpu().numpy()
             target = target_tensor.cpu().numpy()
-            predictions =  np.stack(predictions) # (10, batch_size, 64, 64)
-            predictions = predictions.swapaxes(0,1)  # (batch_size, 10, 64, 64)
+            predictions = np.stack(predictions) 
+            predictions = predictions.swapaxes(0,1)
 
-            mse_batch = np.mean((predictions-target)**2 , axis=(0,1,2)).sum()
-            mae_batch = np.mean(np.abs(predictions-target) ,  axis=(0,1,2)).sum() 
+            mse_batch = np.mean((predictions-target)**2, axis=(0,1,2)).sum()
+            mae_batch = np.mean(np.abs(predictions-target), axis=(0,1,2)).sum() 
             total_mse += mse_batch
             total_mae += mae_batch
             
-            for a in range(0,target.shape[0]):
-                for b in range(0,target.shape[1]):
+            for a in range(0, target.shape[0]):
+                for b in range(0, target.shape[1]):
                     total_ssim += ssim(target[a,b,0,], predictions[a,b,0,]) / (target.shape[0]*target.shape[1]) 
             
             cross_entropy = -target*np.log(predictions) - (1-target) * np.log(1-predictions)
@@ -147,8 +160,8 @@ def evaluate(encoder, loader):
             cross_entropy = cross_entropy / (batch_size*target_length)
             total_bce +=  cross_entropy
      
-    print('eval mse ', total_mse/len(loader),  ' eval mae ', total_mae/len(loader),' eval ssim ',total_ssim/len(loader), ' time= ', time.time()-t0)        
-    return total_mse/len(loader),  total_mae/len(loader), total_ssim/len(loader)
+    print('eval mse ', total_mse/len(val_loader),  ' eval mae ', total_mae/len(val_loader),' eval ssim ',total_ssim/len(val_loader), 'eval time= ', time.time()-t0)        
+    return total_mse/val_len(loader),  total_mae/len(val_loader), total_ssim/len(val_loader)
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -184,7 +197,10 @@ ind = 0
 for i in range(0,7):
     for j in range(0,7):
         constraints[ind,i,j] = 1
-        ind +=1    
+        ind +=1
+
+comment = f' batch_size:{batch_size}'
+writer = SummaryWriter(log_dir='runs/phydnet' ,comment=comment)
 
 phycell  =  PhyCell(input_shape=(64,64), input_dim=64, F_hidden_dims=[49], n_layers=1, kernel_size=(7,7), device=device) 
 convcell =  ConvLSTM(input_shape=(64,64), input_dim=64, hidden_dims=[128,128,64], n_layers=3, kernel_size=(3,3), device=device)   
@@ -193,7 +209,7 @@ encoder  = EncoderRNN(phycell, convcell, device)
 nepochs = 10
 print_every = 1
 eval_every = 1
-save_name = ''
+model_name = ''
 
 train_losses = trainIters(train_loader=train_loader,
                           val_loader=val_loader,
@@ -201,4 +217,9 @@ train_losses = trainIters(train_loader=train_loader,
                           nepochs=nepochs,
                           print_every=print_every,
                           eval_every=eval_every,
-                          model_name=save_name)
+                          checkpoint=False,
+                          model_name=model_name,
+                          writer=writer)
+
+if writer:
+  writer.close()
