ad7be34fcd8fad73afa23c991709601cd0688a49
Franco Mozo <franco.mozo@fing.edu.uy>
2021-08-13 16:30:18 -0300

    :cyclone: gans with tb

--- gan.py
+++ gan.py
@@ -1,18 +1,20 @@
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
-
-from src import preprocessing
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+from src import preprocessing, train
 from src.data import MontevideoFoldersDataset
 from src.dl_models.unet import UNet2
 
 # Hyperparams
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 torch.manual_seed(50)
-BATCH_SIZE = 32
-PT_PATH = None
+BATCH_SIZE = 4
+PT_PATH = '/clusteruy/home03/DeepCloud/deepCloud/checkpoints/10min_UNet2_sigmoid_mae_f32_60_04-08-2021_20:43.pt'
+CSV_PATH='/clusteruy/home03/DeepCloud/deepCloud/data/mvd/train_cosangs_in3_out1.csv'
 LEARNING_RATE = 1e-4
-NUM_EPOCHS = 10
+NUM_EPOCHS = 100
 #CRITIC_ITERATIONS = None
 LAMBDA_GP = 10
 
@@ -45,15 +47,17 @@ train_ds = MontevideoFoldersDataset(path='/clusteruy/home03/DeepCloud/deepCloud/
                                      in_channel=3,
                                      out_channel=1,
                                      min_time_diff=5, max_time_diff=15,
+                                     csv_path=CSV_PATH,
                                      transform=normalize)
 
 train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=2, pin_memory=True)
 
 # Nets
-gen = UNet2(n_channels=3, n_classes=1, bilinear=True,filters=32).to(device)
-disc = UNet2(n_channels=3, n_classes=1, bilinear=True,filters=32).to(device)
+gen = UNet2(n_channels=3, n_classes=1, bilinear=True, filters=32).to(device)
+disc = UNet2(n_channels=1, n_classes=1, bilinear=True, filters=32).to(device)
 gen.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
-disc.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
+#disc.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
+disc.apply(train.weights_init)
 
 # Initializate optimizer
 opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
@@ -62,25 +66,27 @@ opt_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
 gen.train()
 disc.train()
 
+# tb
+writer_real = SummaryWriter(f"runs/gan/gt")
+writer_fake = SummaryWriter(f"runs/gan/pred")
+step = 0
 
 for epoch in range(NUM_EPOCHS):
-    # Target labels not needed! <3 unsupervised
     # for batch_idx, (real, _) in enumerate(train_loader):
     for batch_idx, (in_frames, out_frames) in enumerate(train_loader):
-        in_frames = in_frames.to(device) # this is the noise
-        real = out_frames.to(device) # this is the real
-    
-        cur_batch_size = real.shape[0]
+        in_frames = in_frames.to(device) # this is the noise (B, C, H, W)=(1, 3, 256, 256)
+        out_frames = out_frames.to(device) # this is the real (1, 1, 256, 256))
+        cur_batch_size = out_frames.shape[0]
         # HERE THE PAPER TRAINS A FEW ITER THE DISC BUT I WONT DO THAT
         # Train Critic: max E[critic(real)] - E[critic(fake)]
         # equivalent to minimizing the negative of that
         # for _ in range(CRITIC_ITERATIONS):
         #noise = torch.randn(cur_batch_size, Z_DIM, 1, 1).to(device)
         
-        fake = gen(in_frames)
-        disc_real = disc(real).reshape(-1)
-        disc_fake = disc(fake).reshape(-1)
-        gp = gradient_penalty(disc, real, fake, device=device)
+        pred = gen(in_frames) # (1, 1, 256, 256)
+        disc_real = disc(out_frames).reshape(-1)
+        disc_fake = disc(pred).reshape(-1)
+        gp = gradient_penalty(disc, out_frames, pred, device=device)
         loss_critic = (
             -(torch.mean(disc_real) - torch.mean(disc_fake)) + LAMBDA_GP * gp
         )
@@ -89,7 +95,7 @@ for epoch in range(NUM_EPOCHS):
         opt_disc.step()
 
         # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
-        gen_fake = disc(fake).reshape(-1)
+        gen_fake = disc(pred).reshape(-1)
         loss_gen = -torch.mean(gen_fake)
         gen.zero_grad()
         loss_gen.backward()
@@ -102,8 +108,11 @@ for epoch in range(NUM_EPOCHS):
                   Loss D: {loss_critic:.4f}, loss G: {loss_gen:.4f}"
             )
 
-# PASOS
-# crear dataloader for train
-# levantar alguna unet para arrancar con esos pesos
-# crear las gans
-# entrenar
+            with torch.no_grad():
+                img_grid_real = torchvision.utils.make_grid(out_frames, normalize=True)
+                img_grid_fake = torchvision.utils.make_grid(pred, normalize=True)  
+                
+                writer_real.add_image("gt", img_grid_real, global_step=step)
+                writer_fake.add_image("pred", img_grid_fake, global_step=step)
+
+            step += 1
