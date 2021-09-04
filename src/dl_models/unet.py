93c7f128cf705d6075985f30fa5df19a839d4169
Andres Herrera <97.andres.herrera@gmail.com>
2021-09-04 20:32:40 -0300

    tanh in UNet

--- src/dl_models/unet.py
+++ src/dl_models/unet.py
@@ -102,6 +102,8 @@ class UNet(nn.Module):
                 self.out_activation = nn.Sigmoid()
             if output_activation in ['relu', 'ReLu', 'Relu']:
                 self.out_activation = nn.Hardtanh(min_val=0, max_val=1.0)  #works as relu clip between [0,1]
+            if output_activation in ['tanh']:
+                self.out_activation = nn.Tanh()
         else:
             self.out_activation = nn.Identity()
 
