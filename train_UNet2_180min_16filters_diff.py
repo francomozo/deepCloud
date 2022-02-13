3e1293cb10420b2309e284117a2901733f57b940
Franco Mozo <franco.mozo@fing.edu.uy>
2022-02-13 19:08:48 -0300

    :cyclone: removed 64 filters dictionary retrainFalse

--- train_UNet2_180min_16filters_diff.py
+++ train_UNet2_180min_16filters_diff.py
@@ -1,27 +1,21 @@
 import os
-import time
-
-import cv2 as cv
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
+import time
+import cv2 as cv
+import matplotlib.pyplot as plt
 print('import basic')
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
 print('import torch')
-import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
-
-from src import data, evaluate, model, preprocessing, train, visualization
-from src.data import MontevideoFoldersDataset
-from src.dl_models.unet import UNet, UNet2
-from src.dl_models.unet_advanced import (AttU_Net, NestedUNet, R2AttU_Net,
-                                         R2U_Net)
+from src import data, preprocessing, train
 from src.lib import utils
 
+from src.data import MontevideoFoldersDataset
+from src.dl_models.unet import UNet, UNet2
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 print('finis import')
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -30,67 +24,77 @@ print('using device:', device)
 
 torch.manual_seed(50)
 
-batch_size = 1
-init_filters = 64
-MODEL_PATH = 'checkpoints/R3/60min/60min_UNET__region3_mae_filters64_sigmoid_diffFalse_retrainTrue_76_09-02-2022_17:42.pt' 
-# retrain 1 'checkpoints/R3/60min/60min_UNET__region3_mae_filters64_sigmoid_diffFalse_retrainFalse_40_02-02-2022_21:01.pt' # AGREGAR NOMBRE COMPLETO DEL MODELO
-
-
-
 dataset = 'region3'  # 'mvd', 'uru', 'region3'
 epochs = 100
+batch_size = 6
 
 normalize = preprocessing.normalize_pixels(mean0 = False) #values between [0,1]
 
 train_mvd = MontevideoFoldersDataset(path='/clusteruy/home03/DeepCloud/deepCloud/data/' + dataset + '/train/',    
                                      in_channel=3,
-                                     out_channel=6,
+                                     out_channel=18,
                                      min_time_diff=5,
 				     max_time_diff=15,
+                                     csv_path='/clusteruy/home03/DeepCloud/deepCloud/data/' + dataset + '/train_cosangs_region3.csv',
                                      transform = normalize,
-				     output_last=True)
+				     output_last=True,
+                                     day_pct=1)
                                      
 val_mvd = MontevideoFoldersDataset(path='/clusteruy/home03/DeepCloud/deepCloud/data/' + dataset + '/validation/',    
                                    in_channel=3,
-                                   out_channel=6,
+                                   out_channel=18,
                                    min_time_diff=5,
 				   max_time_diff=15,
+                                   csv_path='/clusteruy/home03/DeepCloud/deepCloud/data/' + dataset + '/val_cosangs_region3.csv',
                                    transform = normalize,
-				   output_last=True)
+				   output_last=True,
+                                   day_pct=1)
 
 train_loader = DataLoader(train_mvd, batch_size=batch_size, shuffle=True, num_workers=2)
 val_loader = DataLoader(val_mvd, batch_size=batch_size, shuffle=True, num_workers=2)
 
-retrain = True
-
+retrain = False
 
 learning_rates = [1e-3]
-arquitecture = ['']  # ['R2', 'Att', 'R2Att', 'Nested']
+arquitecture = ['2']  # ['', '2']
 
+init_filters = 16
+predict_diff = True
 
-grid_search = [ (lr, mdl) for lr in learning_rates for mdl in arquitecture]
+if predict_diff:
+  output_activation = 'tanh'
+else:
+  output_activation = 'sigmoid'
+
+grid_search = [(lr, mdl) for lr in learning_rates for mdl in arquitecture]
 
 for lr, mdl in grid_search:
   if mdl == '':
-    model = UNet(n_channels=3, n_classes=1, bilinear=True, output_activation='sigmoid', filters=init_filters).to(device)
+    model = UNet(n_channels=3, n_classes=1, bilinear=True, output_activation=output_activation, filters=init_filters).to(device)
+  if mdl == '2':
+    model = UNet2(n_channels=3, n_classes=1, bilinear=True, output_activation=output_activation, filters=init_filters).to(device)
+	
   if torch.cuda.device_count() > 1:
     print('Model Paralleling')
     model = nn.DataParallel(model)
 
+  MODEL_PATH = 'checkpoints/R3/180min/'
+
   if retrain:
-    checkpoint = torch.load(MODEL_PATH, map_location=device)
-    if torch.cuda.device_count() == 1:
-        for _ in range(len(checkpoint['model_state_dict'])):
-            key, value = checkpoint['model_state_dict'].popitem(False)
-            checkpoint['model_state_dict'][key[7:] if key[:7] == 'module.' else key] = value
-    model.load_state_dict(checkpoint['model_state_dict']) 
+      checkpoint = torch.load(MODEL_PATH, map_location=device)
+      if torch.cuda.device_count() == 1:
+          for _ in range(len(checkpoint['model_state_dict'])):
+              key, value = checkpoint['model_state_dict'].popitem(False)
+              checkpoint['model_state_dict'][key[7:] if key[:7] == 'module.' else key] = value
+      model.load_state_dict(checkpoint['model_state_dict']) 
 
  
   model.to(device)
   
   if not retrain:
-      model.apply(train.weights_init)  
-  
+    model.apply(train.weights_init)  
+    checkpoint =False
+
   optimizer = optim.Adam(model.parameters(), lr=lr ,betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
  
   if retrain:
@@ -103,13 +107,12 @@ for lr, mdl in grid_search:
   
   train_loss = 'mae'  # ['mae', 'mse', 'ssim']
   loss_for_scheduler = 'mae'
-  predict_diff = False
-  checkpoint_folder = 'R3/60min/'
-  model_name = f'60min_UNET_{mdl}_{dataset}_{train_loss}_filters{init_filters}_sigmoid_diff{predict_diff}_retrain{retrain}'
+  checkpoint_folder = 'R3/180min/'
+  model_name = f'180min_UNET{mdl}_{dataset}_{train_loss}_filters{init_filters}_{output_activation}_diff{predict_diff}_retrain{retrain}'
   
   comment = f' batch_size:{batch_size} lr:{lr} model:{mdl} train_loss:{train_loss} predict_diff{predict_diff}'
   
-  writer = SummaryWriter(log_dir='runs/predict_60min/'+model_name, comment=comment)
+  writer = SummaryWriter(log_dir='runs/predict_180min/'+model_name, comment=comment)
   #writer = None
   print(model_name)
   TRAIN_LOSS, VAL_MAE_LOSS, VAL_MSE_LOSS, VAL_SSIM_LOSS = train.train_model_full(	
