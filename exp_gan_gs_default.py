30b794617b424de663c2563e946eea273af80115
Franco Mozo <fm.login1@gmail.com>
2022-02-19 17:48:06 -0300

    :cyclone: gans

--- exp_gan_gs_default.py
+++ exp_gan_gs_default.py
@@ -1,4 +1,6 @@
+import copy
 import datetime
+import json
 import os
 import random
 
@@ -16,61 +18,79 @@ from src.dl_models.gan import Discriminator
 from src.dl_models.unet import UNet2
 from src.lib.utils import gradient_penalty, save_checkpoint
 
-#####################################################################
-# this script works, but no json is written for later plotting,
-# modify exp_gan_gs_aaap.py instead and use that
-#####################################################################
+# -----------------------------------------------------------------+
+# 19 feb 2022
+# This is the new default script to train gans on mvd
+# Differences:
+#   -> the Unet models have 16 filters DONE
+#   -> models will be trained to 30, 60 and 90 min
+#   -> the dataloaders use csvs in a different way DONE
+#   -> evaluate.evaluate_gan_val evaluation horizon PENDING, debug with pdb
+#   -> the static grid images starts before epoch 1 (and possibly
+#       generate a different dataset with chosen images for this) 
+#   -> modify evaluate.make_val_grid 
+#   -> disable the grid gt vs pred for tb to save time
+#   -> save best model, and model in last epoch
 
-expId = # global experiment Id. string
+
+expId = '' # global experiment Id. string TODO: complete the expId
 
 # Params =======================
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 print(f'Using device {device}')
 torch.manual_seed(50)
-PT_PATH = '/clusteruy/home03/DeepCloud/deepCloud/checkpoints/10min_2_50_09-07-2021_06:21.pt'
-DATA_PATH_TRAIN = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/train/'
-DATA_PATH_VAL = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/validation/'
 
-BATCH_SIZE = 12 # fixed
+PT_PATH = '/clusteruy/home03/DeepCloud/deepCloud/checkpoints/' # TODO:complete path depending on the ph
+DATA_PATH_TRAIN = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/train/'
+CSV_PATH_TRAIN = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/train_cosangs_mvd.csv'
+
+DATA_PATH_VAL = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/validation/'
+CSV_PATH_VAL = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/val_cosangs_mvd.csv' 
+
+BATCH_SIZE = 16 # fixed
 NUM_EPOCHS = 15 # fixed
-predict_horizon = 6 # this is for validation
+PREDICT_HORIZON = 3 # int corresponding to num output images, ph=30min is 3
 sub_expId = 0
 
-# tuneable params:
-lrs = [1e-4, 3e-4] # floats
-critic_iters = [5] # ints
-lambda_gps = [5] # ints
-features_ds = [32] # ints
-load_dict_flags = [True] # bool
-use_critic_iters = [True] # bool
-csv_on_trains = [True] # bool
+outputs = {}
 
-hparams = [(lr, lambda_gp, critic_iter, features_d, load_dict_flag, use_critic_iter, csv_on_train) for lr in lrs
-                                                                                                   for lambda_gp in lambda_gps
-                                                                                                   for critic_iter in critic_iters
-                                                                                                   for features_d in features_ds
-                                                                                                   for load_dict_flag in load_dict_flags
-                                                                                                   for use_critic_iter in use_critic_iters
-                                                                                                   for csv_on_train in csv_on_trains]
-total_exps = len(hparams)
-for index, (lr, lambda_gp, critic_iter, features_d, load_dict_flag, use_critic_iter, csv_on_train) in enumerate(hparams):
-    
+models = {
+    'aaag5' : {
+        'lr' : 0.0001, 'lambda_gp' : 5, 'critic_iter' : 5,  'features_d' : 32, 'use_critic_iter' : True},
+    'aaag8' : {
+        'lr' : 0.0001, 'lambda_gp' : 5, 'critic_iter' : 5,  'features_d' : 32, 'use_critic_iter' : False},
+    'aaah6' : {
+        'lr' : 0.0003, 'lambda_gp' : 5, 'critic_iter' : 5,  'features_d' : 32, 'use_critic_iter' : True},
+    'aaah8' : {
+        'lr' : 0.0003, 'lambda_gp' : 5, 'critic_iter' : 5,  'features_d' : 32, 'use_critic_iter' : False},
+    'aaah10' : {
+        'lr' : 0.0003, 'lambda_gp' : 5, 'critic_iter' : 10,  'features_d' : 16, 'use_critic_iter' : True},
+    'aaah13' : {
+        'lr' : 0.0003, 'lambda_gp' : 5, 'critic_iter' : 10,  'features_d' : 32, 'use_critic_iter' : True},
+    'aaaj4' : {
+        'lr' : 5e-05, 'lambda_gp' : 5, 'critic_iter' : 5,  'features_d' : 16, 'use_critic_iter' : False},
+    'aaaj5' : {
+        'lr' : 5e-05, 'lambda_gp' : 5, 'critic_iter' : 5,  'features_d' : 32, 'use_critic_iter' : True},
+    'aaaj12' : {
+        'lr' : 5e-05, 'lambda_gp' : 5, 'critic_iter' : 10,  'features_d' : 16, 'use_critic_iter' : False},
+    'aaan2' : {
+        'lr' : 5e-05, 'lambda_gp' : 10, 'critic_iter' : 5,  'features_d' : 16, 'use_critic_iter' : True}
+}
+
+total_exps = len(models)
+for index, key in enumerate(models.keys()):    
     sub_expId = index + 1
-
+    
+    lr = models[key]['lr']
+    lambda_gp = models[key]['lambda_gp']
+    critic_iter = models[key]['critic_iter']
+    features_d = models[key]['features_d']
+    use_critic_iter = models[key]['use_critic_iter']
+    
     print(f'=================================================================================================================================')
     print(f'Exp {sub_expId}/{total_exps}.')
-    print(f'ExpId: {expId}{sub_expId}. lr({lr})_lambda_gp({lambda_gp})_critic_iter({critic_iter})_features_d({features_d})_load_dict_flag({load_dict_flag})_use_critic_iter({use_critic_iter})_csv_on_train({csv_on_train}).')
+    print(f'ExpId: {expId}{sub_expId} ({key}). lr({lr})_lambda_gp({lambda_gp})_critic_iter({critic_iter})_features_d({features_d})_use_critic_iter({use_critic_iter}).')
     try:
-        if csv_on_train:
-            CSV_PATH_TRAIN = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/train_cosangs_in3_out1.csv'
-        else:
-            CSV_PATH_TRAIN = None
-        csv_on_val = False # This is fixed for fear comparisons between experiments
-        if csv_on_val:
-            CSV_PATH_VAL = '/clusteruy/home03/DeepCloud/deepCloud/data/mvd/val_cosangs_in3_out6.csv' 
-        else:
-            CSV_PATH_VAL = None
-
         
         # Hyperparams =======================
         LEARNING_RATE = lr
@@ -80,27 +100,33 @@ for index, (lr, lambda_gp, critic_iter, features_d, load_dict_flag, use_critic_i
 
         # Dataloaders =======================
         normalize = preprocessing.normalize_pixels()
-        train_ds = MontevideoFoldersDataset(path=DATA_PATH_TRAIN, in_channel=3, out_channel=1,
-                                            min_time_diff=5, max_time_diff=15,
-                                            csv_path=CSV_PATH_TRAIN,
-                                            transform=normalize)
-        train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=2, pin_memory=True)
+        train_mvd = MontevideoFoldersDataset(path=DATA_PATH_TRAIN, 
+                                             in_channel=3, 
+                                             out_channel=PREDICT_HORIZON,
+                                             min_time_diff=5, 
+                                             max_time_diff=15,
+                                             csv_path=CSV_PATH_TRAIN,
+                                             transform=normalize, 
+                                             output_last=True) 
+        train_loader = DataLoader(train_mvd, batch_size=BATCH_SIZE, shuffle=True, num_workers=2, pin_memory=True)
 
         val_mvd = MontevideoFoldersDataset(path=DATA_PATH_VAL, 
-                                                in_channel=3, out_channel=predict_horizon,
-                                                min_time_diff=5, max_time_diff=15,
-                                                transform=normalize, 
-                                                csv_path=CSV_PATH_VAL)
+                                           in_channel=3, 
+                                           out_channel=PREDICT_HORIZON,
+                                           min_time_diff=5, 
+                                           max_time_diff=15,
+                                           csv_path=CSV_PATH_VAL,
+                                           transform=normalize, 
+                                           output_last=True, 
+                                           csv_path=CSV_PATH_VAL)
 
         val_loader = DataLoader(val_mvd)
 
         # Nets =======================
-        #gen = UNet2(n_channels=3, n_classes=1, bilinear=True, filters=32).to(device)
         disc = Discriminator(channels_img=1, features_d=FEATURES_D).to(device)
-        gen = UNet2(n_channels=3, n_classes=1, bilinear=True, bias=True).to(device)
+        gen = UNet2(n_channels=3, n_classes=1, bilinear=True, bias=True, filters=16).to(device)
         
-        if load_dict_flag:
-            gen.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
+        gen.load_state_dict(torch.load(PT_PATH)["model_state_dict"])
 
         # Initializate optimizer
         opt_gen = optim.RMSprop(gen.parameters(), lr=LEARNING_RATE)
@@ -110,24 +136,27 @@ for index, (lr, lambda_gp, critic_iter, features_d, load_dict_flag, use_critic_i
         disc.train()
 
         # Auxiliar variables =======================
-        criterion = nn.MSELoss()
-        best_val_loss = 1e3
+        best_val_loss = 1e3 
         ts = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M")
         if os.path.isdir(f'{os.getcwd()}/checkpoints/{expId}') == False: 
             os.mkdir(f'{os.getcwd()}/checkpoints/{expId}')
 
         # tb
-        writer_gt = SummaryWriter(f"runs/{expId}/{expId}{sub_expId}/gt")
-        writer_pred = SummaryWriter(f"runs/{expId}/{expId}{sub_expId}/pred")
+        # writer_gt = SummaryWriter(f"runs/{expId}/{expId}{sub_expId}/gt") DISABLE FOR SPEED PURPOSES
+        # writer_pred = SummaryWriter(f"runs/{expId}/{expId}{sub_expId}/pred")
         writer_static = SummaryWriter(f"runs/{expId}/{expId}{sub_expId}/static")
         writer_loss = SummaryWriter(f"runs/{expId}/{expId}{sub_expId}/loss")
         step = 0
 
         gen_loss_by_epochs = []
         disc_loss_by_epochs = []
+        
+        experiment_output = {}
+        
         for epoch in range(NUM_EPOCHS):
             gen_epoch_loss_list = []
             disc_epoch_loss_list = []
+            epoch_output = {}
             print(f'==> Epoch {epoch+1}/{NUM_EPOCHS}') 
             gen.train() 
 
@@ -168,11 +197,10 @@ for index, (lr, lambda_gp, critic_iter, features_d, load_dict_flag, use_critic_i
                         writer_loss.add_scalar('batch_loss/Gen', loss_gen, global_step=step)
                         writer_loss.add_scalar('batch_loss/Disc', loss_disc, global_step=step)
                         
-                        img_grid_gt = torchvision.utils.make_grid(gt.detach()[:4], normalize=True)
-                        img_grid_pred = torchvision.utils.make_grid(pred.detach()[:4], normalize=True)  
-                        
-                        writer_gt.add_image("gt", img_grid_gt, global_step=step)
-                        writer_pred.add_image("pred", img_grid_pred, global_step=step)
+                        # img_grid_gt = torchvision.utils.make_grid(gt.detach()[:4], normalize=True)
+                        #img_grid_pred = torchvision.utils.make_grid(pred.detach()[:4], normalize=True)  
+                        # writer_gt.add_image("gt", img_grid_gt, global_step=step) DISABLE FOR SPEED PURPOSES
+                        # writer_pred.add_image("pred", img_grid_pred, global_step=step)
                     step += 1
                 
                 # for each batch save losses to calculate mean for each epoch 
@@ -185,42 +213,62 @@ for index, (lr, lambda_gp, critic_iter, features_d, load_dict_flag, use_critic_i
 
             # validation each epoch
             gen.eval()
-            val_err_array = evaluate.evaluate_gan_val(gen, val_loader, 
-                                                    predict_horizon=predict_horizon, 
-                                                    device=device,
-                                                    metric='RMSE')
-            val_err_array = np.mean(val_err_array, axis=0) # vector of length 'predict_horizon'
+            val_loss = evaluate.evaluate_gan_val(gen, val_loader, 
+                                                predict_horizon=1, 
+                                                device=device,
+                                                metric='SSIM')
+            val_loss = np.mean(val_loss, axis=0)[0] # single value, varies [-1, 1] 
             
-            print(f'\t -> Disc_epoch_loss: {disc_loss_by_epochs[-1]:.8f}. Gen_epoch_loss: {gen_loss_by_epochs[-1]:.8f}. Val_loss: {val_err_array[0]:.8f}.')
-            print(f'\t -> Val_pred_horiz_loss: {val_err_array}')
+            # for val_loss i want the model to reach 1 in each entry, so im looking to maximize, instead i
+            # will modify so that the best model is when the values go to zero, so first substract 1 so that
+            # [-2, 0] and then multiply by -1 so [2, 0], 0 eq to 1 and 2 to -1
+            val_loss_modified = (val_loss -1)*(-1)
+            
+            print(f'\t -> Disc_epoch_loss: {disc_loss_by_epochs[-1]:.8f}. Gen_epoch_loss: {gen_loss_by_epochs[-1]:.8f}. Val_loss: {val_loss:.8f}.')
 
             # tb losses by epoch
             with torch.no_grad():
-                grid = evaluate.make_val_grid(gen, sequences=3, device=device)
+                grid = evaluate.make_val_grid(gen, sequences=3, device=device, val_mvd=val_mvd)
                 writer_static.add_image("static_imgs", grid, global_step=epoch+1)
                 
                 writer_loss.add_scalar('epoch_loss/Gen', loss_gen, global_step=epoch+1)
                 writer_loss.add_scalar('epoch_loss/Disc', loss_disc, global_step=epoch+1)
-                writer_loss.add_scalar('epoch_loss/Val_RMSE', val_err_array[0], global_step=epoch+1)
+                writer_loss.add_scalar('epoch_loss/Val_SSIM', val_loss, global_step=epoch+1)
 
             # save best model (on val)
-            if val_err_array[0] < best_val_loss:
+            if val_loss_modified < best_val_loss: 
                 if best_val_loss != 1e3:
                     print(f'\t -> New best model!!!')
                 gen_dict = {
                     'epoch': epoch+1,
-                    'model_state_dict': gen.state_dict(),
-                    'opt_state_dict': opt_gen.state_dict(),
+                    'model_state_dict': copy.deepcopy(gen.state_dict()),
+                    'opt_state_dict': copy.deepcopy(opt_gen.state_dict()),
                     'gen_epoch_loss': gen_loss_by_epochs,
                 }
                 disc_dict = {
                         'epoch': epoch+1,
-                        'model_state_dict': disc.state_dict(),
-                        'opt_state_dict': opt_disc.state_dict(),
+                        'model_state_dict': copy.deepcopy(disc.state_dict()),
+                        'opt_state_dict': copy.deepcopy(opt_disc.state_dict()),
                         'disc_epoch_loss': disc_loss_by_epochs,
                 }
-                best_val_loss = val_err_array[0]
+                best_val_loss = val_loss_modified
 
+            epoch_output = {
+                'disc_loss' : disc_loss_by_epochs[-1],
+                'gen_loss' : gen_loss_by_epochs[-1],
+                'val_loss' : val_loss,
+            }
+            
+            actual_epoch = epoch+1
+            experiment_output[actual_epoch] = epoch_output
+
+        exp = expId  + str(sub_expId)
+        outputs[exp] = experiment_output
+        
+        
         save_checkpoint(gen_dict, disc_dict, expId, sub_expId)       
     except:
         print('Experiment failed :(')
+
+with open(f'reports/gan_exp_outputs/{expId}.json', 'w') as fp:
+   json.dump(outputs, fp)
